e46b9ecc523a08eda4f9f8b3a6c9796992e5fb04
Chams-sat <shamssatour14@gmail.com>
2025-07-19 16:58:52 +0100

    pack Updates

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig(({ mode }) => ({
   plugins: [
     react(),
   ],
-  base: "/chams-sat",
+  base: "/chams-sat/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
