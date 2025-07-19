e2ca16f34da4937ca374a5eeab1c6d36009bcc30
Chams-sat <shamssatour14@gmail.com>
2025-07-19 19:49:45 +0100

    Updates

--- src/App.tsx
+++ src/App.tsx
@@ -13,13 +13,11 @@ const App = () => (
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
         <Sonner />
-        <BrowserRouter>
           <Routes>
             <Route path="/" element={<Index />} />
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
             <Route path="*" element={<NotFound />} />
           </Routes>
-        </BrowserRouter>
       </TooltipProvider>
     </QueryClientProvider>
   </React.StrictMode>
