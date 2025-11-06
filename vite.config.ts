a52a5578c228b4c8029e72c682ce3bc5b3dcb915
Chams-sat <shamssatour14@gmail.com>
2025-11-06 01:44:56 +0100

    Updates

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig(({ mode }) => ({
   plugins: [
     react(),
   ],
-  base: "/chams-portfolio",
+  base: "/chams-portfolio/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
